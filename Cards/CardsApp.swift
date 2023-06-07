1fabc824f1dba7cbb3c8f75bb0d867a848ce3db7
artyaz <achmilenko58@gmail.com>
2023-06-07 07:42:54 +0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Артем Чмиленко on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
