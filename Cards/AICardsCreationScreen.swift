55d7e92eb7cd3ca09a37dd0a1f179721fcd8f1f3
artyaz <achmilenko58@gmail.com>
2023-06-23 15:42:24 +0300

    improved animations, redesigned sets adding screen

--- Cards/AICardsCreationScreen.swift
+++ Cards/AICardsCreationScreen.swift
@@ -9,15 +9,51 @@ import SwiftUI
 
 struct AICardsCreationScreen: View {
     var body: some View {
-        VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-            TextField("Placeholder", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-            Button("Done") {
-                        
+        VStack(alignment: .leading, spacing: 30) {
+            HStack(alignment: .top, spacing: 10) {
+                Text("Create card set")
+                    .font(Font.system(size: 40))
+                    .bold()
+                    .foregroundColor(.black)
+                Spacer()
             }
+            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding(10)
+                .frame(width: 335)
+                .cornerRadius(5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .inset(by: 0.50)
+                        .stroke(.black, lineWidth: 0.50)
+                )
+            
+            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding(10)
+                .frame(width: 335)
+                .cornerRadius(5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .inset(by: 0.50)
+                        .stroke(.black, lineWidth: 0.50)
+                )
+            HStack(alignment: .center) {
+                Spacer()
+                Button("Create") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .padding(EdgeInsets(top: 10, leading: 107, bottom: 10, trailing: 107))
+                    .background(.black)
+                    .foregroundColor(.white)
+                    .fontWeight(.medium)
+                    .cornerRadius(10)
+
+                Spacer()
+            }
+            Spacer()
+            
         }
-        
+        .padding(EdgeInsets(top: 50, leading: 25, bottom: 50, trailing: 25))
+        .frame(width: 385, height: 844)
     }
 }
 
--- Cards/CardsHub.swift
+++ Cards/CardsHub.swift
@@ -9,20 +9,33 @@ import SwiftUI
 
 struct CardsHub: View {
     @State private var showActions = false
+    @State private var isTapped = true
+    @State private var shouldShowAICardsCreationScreen = false
     
     var body: some View {
         ZStack {
             CardSetList()
             if showActions {
-                    Color.white.opacity(0.8).ignoresSafeArea().blur(radius: 2)
-                        .onTapGesture {
-                            withAnimation() {
-                                showActions = false
+                Color.white
+                    .opacity(showActions ? 0.8 : 0)
+                    .ignoresSafeArea()
+                    .blur(radius: 2)
+                    //.animation(.easeOut)
+                    .onTapGesture {
+                            let generator = UIImpactFeedbackGenerator(style: .rigid)
+                                    generator.impactOccurred()
+                            self.isTapped.toggle()
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+                                withAnimation{
+                                    self.showActions.toggle()
+                                }
                             }
                         }
                     VStack {
                                 Button(action: {
-                                    // Action 1
+                                    let generator = UIImpactFeedbackGenerator(style: .rigid)
+                                            generator.impactOccurred()
+                                    self.shouldShowAICardsCreationScreen = true
                                 }) {
                                     Text("🖋️").font(Font.system(size: 40))
                                     Text("Create Card Set")
@@ -31,10 +44,19 @@ struct CardsHub: View {
                                         .multilineTextAlignment(.leading)
                                         .foregroundColor(.black) // Text color
                                 }
+                                .sheet(isPresented: $shouldShowAICardsCreationScreen, content: {
+                                            AICardsCreationScreen()
+                                        })
+                                .offset(y: isTapped ? 200 : 0)
+                                .opacity(isTapped ? 0 : 1)
+                                .animation(.easeOut(duration: 0.2))
                                 .scaleEffect(showActions ? 1 : 0)
                                 .animation(Animation.spring(response: 0.5, dampingFraction: 0.3, blendDuration: 0.5))
 
                                 Button(action: {
+                                    let generator = UIImpactFeedbackGenerator(style: .rigid)
+                                            generator.impactOccurred()
+                                    self.isTapped.toggle()
                                     withAnimation {
                                         self.showActions.toggle()
                                     }
@@ -46,6 +68,9 @@ struct CardsHub: View {
                                         .multilineTextAlignment(.leading)
                                         .foregroundColor(.black) // Text color
                                 }
+                                .offset(y: isTapped ? 200 : 0)
+                                .opacity(isTapped ? 0 : 1)
+                                .animation(.easeOut(duration: 0.2))
                                 .scaleEffect(showActions ? 1 : 0)
                                 .animation(Animation.spring(response: 0.49, dampingFraction: 0.29, blendDuration: 0.49))
                             }
@@ -53,9 +78,13 @@ struct CardsHub: View {
             }
 
             Button(action: {
+                let generator = UIImpactFeedbackGenerator(style: .rigid)
+                        generator.impactOccurred()
                     withAnimation() { // Modify this line
                         showActions.toggle()
+                        isTapped.toggle()
                     }
+                
                 }) {
                 Image(systemName: "plus")
                     .font(.system(size: 24, weight: .bold))
